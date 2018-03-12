2fd3d56055a3c224d2626051335edc907f0102d8
Jesper Engman <jpengman@gmail.com>
2018-03-12 09:22:25 +0100

    adding loading icon

--- src/main/webapp/scripts/contentHandler.js
+++ src/main/webapp/scripts/contentHandler.js
@@ -4,61 +4,76 @@
 google.load('visualization', '1', {
 	'packages' : [ 'corechart' ]
 });
-function drawChart(url, targetDivId, options) {
-	var chartDiv = document.getElementById(targetDivId);
-	$("#content").height($(window).height() - 150);
+function drawChart(url, options) {
+	var contentDiv= $("#content");
+	contentDiv.height($(window).height() - 150);
 	if (dataFetched == false) {
-		jsonData = $.ajax({
+		contentDiv.html($("#tmp_cog").clone().attr("id","cog"));
+		$.ajax({
 			url : baseurl + url,
 			dataType : "json",
-			async : false,
 			beforeSend : function(xhr) {
 				xhr.setRequestHeader("Authorization",
 						"Basic d2ViOkhlbWxpZzEyMyE=");
-			}
-		}).responseText;
+			},
+			success : function(result) {
+				jsonData = result;
+				var data = new google.visualization.DataTable(jsonData);
+				var chart = new google.visualization.LineChart( document.getElementById("content"));
+				chart.draw(data, options);
+			},
+			error : function(xhr,status,error) {
+				contentDiv.html(error);
+				}
+		});
 		dataFetched = true;
+	}else{
+		var data = new google.visualization.DataTable(jsonData);
+		var chart = new google.visualization.LineChart( document.getElementById("content"));
 	}
-	var data = new google.visualization.DataTable(jsonData);
-	var chart = new google.visualization.LineChart(chartDiv);
-	chart.draw(data, options);
+	
 }
 
 function drawTanks() {
-	var accsvg = $
-			.ajax({
-				url : baseurl + "svg/heating",
-				dataType : "svg",
-				async : false,
-				beforeSend : function(xhr) {
-					xhr.setRequestHeader("Authorization",
-							"Basic d2ViOkhlbWxpZzEyMyE=");
-				}
-			}).responseText;
-	document.getElementById('content').innerHTML = accsvg;
+	if (dataFetched == false) {
+		var contentDiv= $("#content");
+		contentDiv.html($("#tmp_cog").clone().attr("id","cog"));
+		$.ajax({
+		url : baseurl + "svg/heating",
+		beforeSend : function(xhr) {
+			xhr.setRequestHeader("Authorization", "Basic d2ViOkhlbWxpZzEyMyE=");
+		},
+		success : function(result) {
+			contentDiv.html(result);
+		},
+		error : function(xhr,status,error) {
+			contentDiv.html(error);
+			}
+		});
+		dataFetched = true;
+	}
 }
 function drawContent() {
-	//Drawing content
+	// Drawing content
 	if (content == 'chartByID') {
-		drawChart(requestByID + request + '/' + time, 'content', options);
+		drawChart(requestByID + request + '/' + time, options);
 		showSlider();
 	} else if (content == 'chartByType') {
-		drawChart(requestByType + request + '/' + time, 'content', options);
+		drawChart(requestByType + request + '/' + time, options);
 		showSlider();
 	} else if (content == 'MinMaxAvg') {
-		drawChart(requestMinMaxAvg + request + '/14', 'content', options);
+		drawChart(requestMinMaxAvg + request + '/14', options);
 		hideSlider();
 	} else if (content == 'tanks') {
 		drawTanks();
 		hideSlider();
 	}
-	//Enable/disable Timer
-	if(content=='MinMaxAvg'){
-		timer=null;
-	}else{
-		setInterval(drawContentTimer, 60000);
+	// Enable/disable Timer
+	clearInterval(timer);
+	if (content != 'MinMaxAvg') {
+			timer = setInterval(drawContentTimer, 60000);
 	}
-	//Set Content Header
+	// Set Content Header
 	document.getElementById('content_header').innerHTML = contentHeader;
 }
 function drawContentWith(contentIn, contentHeaderIn, requestIn) {
